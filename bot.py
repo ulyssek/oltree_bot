a23ae3526822328677ee96e1383d0dec1d567b4b
Arnaud A <arnaud@ruuand.fr>
2020-04-26 14:29:34 +0200

    Modification module bagarre

--- bot.py
+++ bot.py
@@ -1,6 +1,3 @@
-
-
-
 from config import token
 
 import discord
@@ -8,12 +5,8 @@ import numpy as np
 import random
 import json
 
-
-
-
 def get_randint(bound):
     return random.randint(1,bound)
-
 
 def roll_a_dice(bound,explode=True,value=0):
     dice = get_randint(bound)
@@ -29,11 +22,9 @@ def roll_n_dices(n,bound,explode=True):
         rolls[i] = roll_a_dice(bound,explode,0)
     return rolls
 
-
 def get_sum(rolls):
     sorted_rolls = np.sort(rolls)
     return int(sum(sorted_rolls[-2:]))
-
 
 def all_cards(cards):
     all_cards = []
@@ -42,13 +33,12 @@ def all_cards(cards):
     return all_cards
 
 file_name = "cards.json"
+players_json = "players.json"
 
 def store_cards(client):
     cards = client.stored_values["cards"]
     with open(file_name, 'w') as fp:
         json.dump(cards, fp)
-
-
 
 def send_card(channel,card_number):
         with open('images/Cartes-' + str(card_number) + '.png', 'rb') as fp:
@@ -59,50 +49,83 @@ def get_player_name(message):
     player = str(message.author).split("#")[0]
     return player
 
-
 def sort_cards(client):
     for player in client.stored_values["cards"]:
         client.stored_values["cards"][player].sort()
-
 
 async def cmd_hello(message):
     """Un p'tit coucou"""
     await message.channel.send("Fais pas le malin mon p'tit gars")
 
 async def cmd_bagarre(message):
-    """Faire un jet de dé (maitrise, prouesse, exhaltation)"""
+    """Faire un jet de dé (maitrise, prouesse, exaltation)"""
+    dice_value = client.stored_values["dice_value"]
+    bonus_touch, bonus_dmg, jet = parse_bagarre(message) 
     rolls = roll_n_dices(3,dice_value,False)
     stored_dices = client.stored_values["dices"] = rolls
     ace = client.stored_values["ace"] = (rolls == dice_value)
-    await message.channel.send(" ".join(map(lambda x : str(int(x)),rolls)))
-    await message.channel.send("Total : " + str(get_sum(stored_dices)))
-        for i in range(sum(ace)):
-            with open('images/boum.jpg', 'rb') as fp:
-                f = discord.File(fp)
-                await message.channel.send(file=f)
+    msg_bagarre = format_bagarre(message, jet, rolls, bonus_touch, bonus_dmg)
+    await message.channel.send(msg_bagarre)
+    for i in range(sum(ace)):
+        with open('images/boum.jpg', 'rb') as fp:
+            f = discord.File(fp)
+            await message.channel.send(file=f)
+
+def parse_bagarre(message):
+    values = message.content.split()
+    classes =  ["soldat", "voyageur", "érudit", "archer", "assassin", "berzekr", "guerrier"]
+    player = get_player_name(message)
+    bonus_touch, bonus_dmg, jet = 0, 0, None
+    if len(values) > 1:
+        jet = values[1]
+        if jet in classes:
+            if jet in ["soldat", "voyageur", "érudit"]:
+                bonus_touch = client.stored_values["players"][player][jet]
+            else:
+                bonus_touch = client.stored_values["players"][player]["soldat"]
+                if jet == "berzekr":
+                    bonus_touch += client.stored_values["players"][player]["berzekr"]
+                    bonus_dmg = client.stored_values["players"][player]["berzekr"]
+                elif jet in ["guerrier", "archer"]:
+                    bonus_dmg = client.stored_values["players"][player][jet]
+    return bonus_touch, bonus_dmg, jet
+
+def format_bagarre(message, jet, rolls, bonus_touch, bonus_dmg):
+    rolls_txt = " ".join(map(lambda x : str(int(x)),rolls))
+    mait, prou, exalt = map(lambda x: int(x), rolls)
+    msg = "Jet: %s (Bonus touche: %d, Bonus dégâts: %d)\n" % (rolls_txt, bonus_touch, bonus_dmg)
+    msg += "%d (%d dégâts) ou %d (%d dégâts) ou %d (%d dégâts)" % (
+            mait + prou + bonus_touch,
+            mait + bonus_dmg,
+            mait + exalt + bonus_touch,
+            mait + bonus_dmg,
+            exalt + prou + bonus_touch,
+            exalt + bonus_dmg)
+    return msg
 
 async def cmd_explode(message):
     """Relance les dés qui ont explosé au précédent jet du joueur"""
+    dice_value = client.stored_values["dice_value"]
     if "ace" in client.stored_values.keys():
         ace = client.stored_values["ace"]
     else:
         ace = [0]
-   if sum(ace) == 0:
-       await message.channel.send("Fais pas le malin mon p'tit gars")
-       return
+    if sum(ace) == 0:
+        await message.channel.send("Fais pas le malin mon p'tit gars")
+        return
 
-   stored_dices = client.stored_values["dices"]
-   exploding_dices = roll_n_dices(sum(ace),dice_value,False)
-   stored_dices[ace] += exploding_dices
-   ace[ace] &= (exploding_dices == dice_value)
-   await message.channel.send(" ".join(map(lambda x : str(int(x)),exploding_dices)) + " (New roll)")
-   await message.channel.send(" ".join(map(lambda x : str(int(x)),stored_dices)) + " (Total)")
-   if sum(ace) == 0:
-       await message.channel.send("Total : " + str(get_sum(stored_dices)))
-       for i in range(sum(ace)):
-           with open('images/boum.jpg', 'rb') as fp:
-               f = discord.File(fp)
-               await message.channel.send(file=f)
+    stored_dices = client.stored_values["dices"]
+    exploding_dices = roll_n_dices(sum(ace),dice_value,False)
+    stored_dices[ace] += exploding_dices
+    ace[ace] &= (exploding_dices == dice_value)
+    await message.channel.send(" ".join(map(lambda x : str(int(x)),exploding_dices)) + " (New roll)")
+    await message.channel.send(" ".join(map(lambda x : str(int(x)),stored_dices)) + " (Total)")
+    if sum(ace) == 0:
+        await message.channel.send("Total : " + str(get_sum(stored_dices)))
+        for i in range(sum(ace)):
+            with open('images/boum.jpg', 'rb') as fp:
+                f = discord.File(fp)
+                await message.channel.send(file=f)
 
 async def cmd_test(message):
     """Une commande de test"""
@@ -220,7 +243,6 @@ commands = {
 
 client = discord.Client()
 
-
 client.stored_values = {
         "count" : 0,
         "dice_value" : 8,
@@ -229,27 +251,35 @@ client.stored_values = {
 with open(file_name) as json_file:
     client.stored_values["cards"] = json.load(json_file)
 
+
+with open(players_json) as json_file:
+    client.stored_values["players"] = json.load(json_file)
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
-
+    if message.author == client.user:
+        return
     content = message.content
-    start = content.startswith
-    dice_value = client.stored_values["dice_value"]
+    command = content.split()[0]
     channel = message.channel
     if message.author == client.user:
         return
 
     print(get_player_name(message) + ' - ' + str(content))
+    
+    if command not in commands.keys() and command != ";help":
+        await message.channel.send("Ca marcherait mieux si tu regardais ton clavier en tapant")
+        return
 
-    if start(';help'):
-        for cmd, func in commands:
-            await channel.send(cmd + ' ' + func.__doc__)
+    if command == ";help":
+        help_msg = "\n".join([cmd + " " + func.__doc__ for cmd,func in commands.items()])
+        await channel.send(help_msg)
     else:
-        return await commands[message.content](message)
+        return await commands[command](message)
 
 
 client.run(token)
