d8aacd71cd21bb0d566d7073f89df855e23e9ae3
ulyssek <ulysseklatzmann@gmail.com>
2020-04-26 21:49:03 +0200

    toolbox file added

--- bot.py
+++ bot.py
@@ -1,58 +1,16 @@
 from config import token
 from config import players
+from toolbox import *
 
 import discord
 import numpy as np
 import random
 import json
 
-def get_randint(bound):
-    return random.randint(1,bound)
 
-def roll_a_dice(bound,explode=True,value=0):
-    dice = get_randint(bound)
-
-    value += dice
-    if explode & (dice == bound):
-        return roll_a_dice(bound,True,value)
-    return value
-
-def roll_n_dices(n,bound,explode=True):
-    rolls = np.zeros(n)
-    for i in range(n):
-        rolls[i] = roll_a_dice(bound,explode,0)
-    return rolls
-
-def get_sum(rolls):
-    sorted_rolls = np.sort(rolls)
-    return int(sum(sorted_rolls[-2:]))
-
-def all_cards(cards):
-    all_cards = []
-    for key in cards.keys():
-        all_cards.extend(cards[key])
-    return all_cards
 
 file_name = "cards.json"
 players_json = "players.json"
-
-def store_cards(client):
-    cards = client.stored_values["cards"]
-    with open(file_name, 'w') as fp:
-        json.dump(cards, fp)
-
-def send_card(channel,card_number):
-        with open('images/Cartes-' + str(card_number) + '.png', 'rb') as fp:
-            f = discord.File(fp)
-            return channel.send(file=f)
-
-def get_player_name(message):
-    player = str(message.author).split("#")[0]
-    return player
-
-def sort_cards(client):
-    for player in client.stored_values["cards"]:
-        client.stored_values["cards"][player].sort()
 
 async def cmd_hello(message):
     """Un p'tit coucou"""
@@ -330,6 +288,7 @@ weapons_dict = {
 client = discord.Client()
 
 classes =  ["soldat", "voyageur", "érudit", "archer", "assassin", "berzekr", "guerrier"]
+
 client.stored_values = {
         "count" : 0,
         "dice_value" : 8,
---
+++ toolbox.py
@@ -0,0 +1,54 @@
+import numpy as np
+import random
+import json
+
+
+
+
+
+def get_randint(bound):
+    return random.randint(1,bound)
+
+def roll_a_dice(bound,explode=True,value=0):
+    dice = get_randint(bound)
+
+    value += dice
+    if explode & (dice == bound):
+        return roll_a_dice(bound,True,value)
+    return value
+
+def roll_n_dices(n,bound,explode=True):
+    rolls = np.zeros(n)
+    for i in range(n):
+        rolls[i] = roll_a_dice(bound,explode,0)
+    return rolls
+
+def get_sum(rolls):
+    sorted_rolls = np.sort(rolls)
+    return int(sum(sorted_rolls[-2:]))
+
+def all_cards(cards):
+    all_cards = []
+    for key in cards.keys():
+        all_cards.extend(cards[key])
+    return all_cards
+
+def store_cards(client):
+    cards = client.stored_values["cards"]
+    with open(file_name, 'w') as fp:
+        json.dump(cards, fp)
+
+def send_card(channel,card_number):
+        with open('images/Cartes-' + str(card_number) + '.png', 'rb') as fp:
+            f = discord.File(fp)
+            return channel.send(file=f)
+
+def get_player_name(message):
+    player = str(message.author).split("#")[0]
+    return player
+
+def sort_cards(client):
+    for player in client.stored_values["cards"]:
+        client.stored_values["cards"][player].sort()
+
+
